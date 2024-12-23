4bc93f86e6fd0d04b085aafcc6b78b7fc2e3909c
unknown <mandyatwork24@gmail.com>
2024-12-23 19:55:28 +0530

    browsers

---
+++ src/basics/MultipleBrowsers.java
@@ -0,0 +1,35 @@
+package basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MultipleBrowsers {
+
+	public static void main(String[] args) {
+		
+		String browser= "firefox";
+		WebDriver driver= null;
+		
+		
+		if(browser.equalsIgnoreCase("chrome")){
+			driver = new ChromeDriver(); 	
+		}
+		else if(browser.equalsIgnoreCase("firefox")) {
+		driver = new FirefoxDriver(); 
+		}
+		else if(browser.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver(); 
+		}
+		else {
+			System.out.println("Please choose correct diver");
+		}
+		
+		driver.manage().window().maximize();
+		driver.get("https://www.flipkart.com/");	
+		driver.quit();
+
+	}
+
+}
